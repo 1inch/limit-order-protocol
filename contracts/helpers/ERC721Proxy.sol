a1307bc34dcc923c4ba7fffe3f822e824a6bb774
Mikhail Melnik <by.zumzoom@gmail.com>
2021-04-02 23:19:08 +0300

    use correct selectors

--- contracts/helpers/ERC721Proxy.sol
+++ contracts/helpers/ERC721Proxy.sol
@@ -10,13 +10,13 @@ import "./ImmutableOwner.sol";
 /* solhint-disable func-name-mixedcase */
 
 abstract contract ERC721Proxy is ImmutableOwner {
-    // func_4002L9TKH(address,address,uint256,address) = transferFrom + 2 = 0x8d076e87
-    function func_4002L9TKH(address from, address to, uint256 tokenId, IERC721 token) external onlyImmutableOwner {
+    // func_40aVqeY(address,address,uint256,address) = transferFrom + 2 = 0x23b872df
+    function func_40aVqeY(address from, address to, uint256 tokenId, IERC721 token) external onlyImmutableOwner {
         token.transferFrom(from, to, tokenId);
     }
 
-    // func_2000nVqcj(address,address,uint256,address) == transferFrom + 3 = 0x8d076e88
-    function func_2000nVqcj(address from, address to, uint256 tokenId, IERC721 token) external onlyImmutableOwner {
+    // func_20xtkDI(address,address,uint256,address) == transferFrom + 3 = 0x23b872e0
+    function func_20xtkDI(address from, address to, uint256 tokenId, IERC721 token) external onlyImmutableOwner {
         token.safeTransferFrom(from, to, tokenId);
     }
 }
