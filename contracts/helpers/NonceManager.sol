3ce6693c35a769ecc1039fb81ceea31c69c08a32
Mikhail Melnik <by.zumzoom@gmail.com>
2021-05-04 14:10:40 +0300

    simplify nonce manage

--- contracts/helpers/NonceManager.sol
+++ contracts/helpers/NonceManager.sol
@@ -6,23 +6,19 @@ pragma solidity ^0.8.0;
 contract NonceManager {
     event NonceIncreased(address indexed maker, uint256 newNonce);
 
-    mapping(address => uint256) private _nonces;
-
-    function nonce(address makerAddress) external view returns(uint256) {
-        return _nonces[makerAddress];
-    }
+    mapping(address => uint256) public nonce;
 
     function increaseNonce() external {
         advanceNonce(1);
     }
 
     function advanceNonce(uint8 amount) public {
-        uint256 newNonce = _nonces[msg.sender] + amount;
-        _nonces[msg.sender] = newNonce;
+        uint256 newNonce = nonce[msg.sender] + amount;
+        nonce[msg.sender] = newNonce;
         emit NonceIncreased(msg.sender, newNonce);
     }
 
     function nonceEquals(address makerAddress, uint256 makerNonce) external view returns(bool) {
-        return _nonces[makerAddress] == makerNonce;
+        return nonce[makerAddress] == makerNonce;
     }
 }
