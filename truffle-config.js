464da58d0fe7a69da249b5707adcdb692c60e27d
Anton Bukov <k06aaa@gmail.com>
2021-05-05 11:59:53 +0200

    Support compressed signatures via OpenZeppelin 4.1.0

--- truffle-config.js
+++ truffle-config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     compilers: {
         solc: {
-            version: '0.8.3',
+            version: '0.8.4',
             settings: {
                 optimizer: {
                     enabled: true,
