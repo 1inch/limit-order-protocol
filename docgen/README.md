4c8b258903da084795e1d15e9d108f6323d981ff
Psilon <pzixel@gmail.com>
2021-06-07 13:25:38 +0300

    initial docgen commit

---
+++ docgen/README.md
@@ -0,0 +1,3 @@
+# 1inch limit order protocol
+
+https://github.com/1inch/limit-order-protocol
---
+++ docify.js
@@ -0,0 +1,36 @@
+const INPUT_DIR          = "contracts";
+const BASE_DIR           = "docgen";
+const OUTPUT_DIR         = "docgen/docs";
+const SOLC_NPM_NAME      = "solc";
+
+const fs        = require("fs");
+const path      = require("path");
+const spawn     = require('cross-spawn')
+
+function runProcess(name, args) {
+    console.log(`running ${name} with args ${JSON.stringify(args)}`);
+    const result = spawn.sync(name, args, {stdio: ["inherit", "inherit", "pipe"]});
+    if (result.stderr.length > 0)
+        throw new Error(result.stderr);
+}
+
+function generateGitbookFiles() {
+    fs.copyFileSync(path.join(BASE_DIR, "README.md"), path.join(OUTPUT_DIR, "README.md"))
+}
+
+const solidityDocgenArgs = [
+    "solidity-docgen",
+    "-i",
+    INPUT_DIR,
+    "-o",
+    OUTPUT_DIR,
+    "--solc-module",
+    SOLC_NPM_NAME,
+    "--solc-settings",
+    JSON.stringify({optimizer: {enabled: false}}),
+    "--templates",
+    BASE_DIR
+];
+
+runProcess("npx", solidityDocgenArgs);
+generateGitbookFiles();
