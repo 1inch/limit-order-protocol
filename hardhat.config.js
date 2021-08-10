edb5bfb734e40a9eada267add6c8c762742c6987
Psilon <pzixel@gmail.com>
2021-08-10 20:39:03 +0300

    Add optimism to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,16 @@
+require('@eth-optimism/hardhat-ovm');
 require('@nomiclabs/hardhat-truffle5');
 require('solidity-coverage');
 require('hardhat-deploy');
 require('hardhat-gas-reporter');
 
 module.exports = {
+    ovm: {
+        solcVersion: '0.7.6+commit.3b061308',
+    },
+    paths : {
+        artifacts: './artifacts-ovm'
+    },
     solidity: {
         version: '0.7.6',
         settings: {
