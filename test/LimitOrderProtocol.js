43b27cdd4d56c0fc5d533c320c743aee1090b600
Mikhail Melnik <by.zumzoom@gmail.com>
2021-11-09 14:18:10 +0800

    [M02] use explicit tokenId parameter

--- test/LimitOrderProtocol.js
+++ test/LimitOrderProtocol.js
@@ -314,8 +314,8 @@ describe('LimitOrderProtocol', async function () {
         await this.weth.approve(erc721proxy.address, '10');
 
         const order = buildOrder(this.swap, erc721proxy, erc721proxy, 10, 10);
-        order.makerAssetData = '0x' + erc721proxy.contract.methods.func_602HzuS(wallet, constants.ZERO_ADDRESS, 10, this.dai.address).encodeABI().substr(202);
-        order.takerAssetData = '0x' + erc721proxy.contract.methods.func_602HzuS(constants.ZERO_ADDRESS, wallet, 10, this.weth.address).encodeABI().substr(202);
+        order.makerAssetData = '0x' + erc721proxy.contract.methods.func_60iHVgK(wallet, constants.ZERO_ADDRESS, 0, 10, this.dai.address).encodeABI().substr(202);
+        order.takerAssetData = '0x' + erc721proxy.contract.methods.func_60iHVgK(constants.ZERO_ADDRESS, wallet, 0, 10, this.weth.address).encodeABI().substr(202);
 
         const data = buildOrderData(this.chainId, this.swap.address, order);
         const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
