3811d43a977d834e3de5c97fe6bfb7f0760c8ffe
Mikhail Melnik <by.zumzoom@gmail.com>
2021-11-09 16:29:52 +0800

    [L14] use latest aggregator interface

--- contracts/mocks/AggregatorMock.sol
+++ contracts/mocks/AggregatorMock.sol
@@ -3,21 +3,63 @@
 pragma solidity 0.8.9;
 pragma abicoder v1;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
 
 /// @title Mock oracle that always returns specified token price
-contract AggregatorMock is AggregatorInterface {
+contract AggregatorMock is AggregatorV2V3Interface {
     int256 private immutable _answer;
 
     constructor(int256 answer) {
         _answer = answer;
     }
 
-    function latestAnswer() external view override returns (int256) {
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    function description() external pure returns (string memory) {
+        return "AggregatorMock";
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        require(_roundId == 0, "No data present");
+        return latestRoundData();
+    }
+
+    function latestRoundData()
+        public
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        // solhint-disable-next-line not-rely-on-time
+        return (0, _answer, block.timestamp - 100, block.timestamp - 100, 0);
+    }
+
+    function latestAnswer() public view returns (int256) {
         return _answer;
     }
 
-    function latestTimestamp() external view override returns (uint256) {
+    function latestTimestamp() public view returns (uint256) {
         // solhint-disable-next-line not-rely-on-time
         return block.timestamp - 100;
     }
@@ -27,13 +69,12 @@ contract AggregatorMock is AggregatorInterface {
     }
 
     function getAnswer(uint256 roundId) external view returns (int256) {
-        require(roundId == 0, "Missing roundId");
-        return _answer;
+        require(roundId == 0, "No data present");
+        return latestAnswer();
     }
 
     function getTimestamp(uint256 roundId) external view returns (uint256) {
-        require(roundId == 0, "Missing roundId");
-        // solhint-disable-next-line not-rely-on-time
-        return block.timestamp - 100;
+        require(roundId == 0, "No data present");
+        return latestTimestamp();
     }
 }
