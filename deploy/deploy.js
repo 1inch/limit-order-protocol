1a9eb9c848ed2b51656f4fb7c3ac398aaafeed78
Mikhail Melnik <by.zumzoom@gmail.com>
2021-11-26 17:17:21 +0800

    update deploy script

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,6 +1,9 @@
 const hre = require('hardhat');
 const { getChainId } = hre;
 
+// const MAX_FEE_PER_GAS = 150e9;
+// const MAX_PRIORITY_FEE_PER_GAS = 2e9;
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running deploy script');
     console.log('network id ', await getChainId());
@@ -11,16 +14,18 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const limitOrderProtocol = await deploy('LimitOrderProtocol', {
         from: deployer,
         skipIfAlreadyDeployed: true,
+        // maxFeePerGas: MAX_FEE_PER_GAS,
+        // maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
     });
     await limitOrderProtocol;
 
     console.log('LimitOrderProtocol deployed to:', limitOrderProtocol.address);
 
-    // if (await getChainId() !== '31337') {
-    //     await hre.run('verify:verify', {
-    //         address: limitOrderProtocol.address,
-    //     });
-    // }
+    if (await getChainId() !== '31337') {
+        await hre.run('verify:verify', {
+            address: limitOrderProtocol.address,
+        });
+    }
 };
 
 module.exports.skip = async () => true;
