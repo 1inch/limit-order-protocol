70f59a22af9f07d44e4dfec3a56a98be5ac309d7
Mikhail Melnik <by.zumzoom@gmail.com>
2021-11-29 12:57:40 +0800

    add deployments

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,9 +1,6 @@
 const hre = require('hardhat');
 const { getChainId } = hre;
 
-// const MAX_FEE_PER_GAS = 150e9;
-// const MAX_PRIORITY_FEE_PER_GAS = 2e9;
-
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running deploy script');
     console.log('network id ', await getChainId());
@@ -14,8 +11,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const limitOrderProtocol = await deploy('LimitOrderProtocol', {
         from: deployer,
         skipIfAlreadyDeployed: true,
-        // maxFeePerGas: MAX_FEE_PER_GAS,
-        // maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
     });
     await limitOrderProtocol;
 
