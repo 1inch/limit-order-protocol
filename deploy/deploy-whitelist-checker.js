2d9204769623804ef7d441cb45745c78fe7201e1
Mikhail Melnik <by.zumzoom@gmail.com>
2022-05-23 12:13:03 +0200

    new deployment

--- deploy/deploy-whitelist-checker.js
+++ deploy/deploy-whitelist-checker.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 const { getChainId } = hre;
 
-const REGISTRY = '0xFC0E24F6Fe4765C8996591F5D3E43b7060ABa83B';
+const REGISTRY = '0xAc8D32a117799d58C5c10C7c23a9cD05f8Ce4F35';
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running deploy script');
