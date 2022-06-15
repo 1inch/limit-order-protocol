04b6499bfe02c2c64d764b8cc0e585274a3efb50
Anton Bukov <k06aaa@gmail.com>
2022-06-15 15:29:51 +0200

    Refactor ArgumentsDecoder

--- contracts/libraries/ArgumentsDecoder.sol
+++ contracts/libraries/ArgumentsDecoder.sol
@@ -5,46 +5,34 @@ pragma abicoder v1;
 
 /// @title Library with gas efficient alternatives to `abi.decode`
 library ArgumentsDecoder {
-    function decodeUint256Memory(bytes memory data) internal pure returns(uint256) {
-        uint256 value;
+    function decodeUint256Memory(bytes memory data) internal pure returns(uint256 value) {
         assembly { // solhint-disable-line no-inline-assembly
             value := mload(add(data, 0x20))
         }
-        return value;
     }
 
-    function decodeUint256(bytes calldata data) internal pure returns(uint256) {
-        uint256 value;
+    function decodeUint256(bytes calldata data) internal pure returns(uint256 value) {
         assembly { // solhint-disable-line no-inline-assembly
             value := calldataload(data.offset)
         }
-        return value;
     }
 
-    function decodeBoolMemory(bytes memory data) internal pure returns(bool) {
-        bool value;
+    function decodeBoolMemory(bytes memory data) internal pure returns(bool value) {
         assembly { // solhint-disable-line no-inline-assembly
             value := eq(mload(add(data, 0x20)), 1)
         }
-        return value;
     }
 
-
-    function decodeBool(bytes calldata data) internal pure returns(bool) {
-        bool value;
+    function decodeBool(bytes calldata data) internal pure returns(bool value) {
         assembly { // solhint-disable-line no-inline-assembly
             value := eq(calldataload(data.offset), 1)
         }
-        return value;
     }
 
-    function decodeTargetAndCalldata(bytes calldata data) internal pure returns(address, bytes calldata) {
-        address target;
-        bytes calldata args;
+    function decodeTargetAndCalldata(bytes calldata data) internal pure returns(address target, bytes calldata args) {
         assembly {  // solhint-disable-line no-inline-assembly
             target := shr(96, calldataload(data.offset))
         }
         args = data[20:];
-        return (target, args);
     }
 }
