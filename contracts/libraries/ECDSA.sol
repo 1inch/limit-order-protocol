05e5a5435beb016bf83729ff1dc18175b098e090
Denis <zzomrot.ne@gmail.com>
2022-06-24 00:18:19 +0300

    Setted memory-safe-assembly in ECDSA

--- contracts/libraries/ECDSA.sol
+++ contracts/libraries/ECDSA.sol
@@ -91,7 +91,7 @@ library ECDSA {
         // (bool success, bytes memory data) = signer.staticcall(abi.encodeWithSelector(IERC1271.isValidSignature.selector, hash, signature));
         // return success && data.length >= 4 && abi.decode(data, (bytes4)) == IERC1271.isValidSignature.selector;
         bytes4 selector = IERC1271.isValidSignature.selector;
-        // TODO: set memory-safe-assembly
+        /// @solidity memory-safe-assembly
         assembly { // solhint-disable-line no-inline-assembly
             let ptr := mload(0x40)
             let len := add(0x64, signature.length)
@@ -113,7 +113,7 @@ library ECDSA {
         // (bool success, bytes memory data) = signer.staticcall(abi.encodeWithSelector(IERC1271.isValidSignature.selector, hash, abi.encodePacked(r, vs)));
         // return success && data.length >= 4 && abi.decode(data, (bytes4)) == IERC1271.isValidSignature.selector;
         bytes4 selector = IERC1271.isValidSignature.selector;
-        // TODO: set memory-safe-assembly
+        /// @solidity memory-safe-assembly
         assembly { // solhint-disable-line no-inline-assembly
             let ptr := mload(0x40)
             let len := add(0x64, 64)
@@ -136,7 +136,7 @@ library ECDSA {
         // (bool success, bytes memory data) = signer.staticcall(abi.encodeWithSelector(IERC1271.isValidSignature.selector, hash, abi.encodePacked(r, vs & ~uint256(1 << 255), uint8(vs >> 255))));
         // return success && data.length >= 4 && abi.decode(data, (bytes4)) == IERC1271.isValidSignature.selector;
         bytes4 selector = IERC1271.isValidSignature.selector;
-        // TODO: set memory-safe-assembly
+        /// @solidity memory-safe-assembly
         assembly { // solhint-disable-line no-inline-assembly
             let ptr := mload(0x40)
             let len := add(0x64, 65)
