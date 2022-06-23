3eb25bcbc18fe8fa4a903705ce833813df14e155
Gleb Alekseev <alekseev.gleb@gmail.com>
2022-06-23 21:35:33 +0300

    basic description

--- contracts/LimitOrderProtocol.sol
+++ contracts/LimitOrderProtocol.sol
@@ -6,12 +6,34 @@ import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/draft-EIP712.
 import "./OrderMixin.sol";
 import "./OrderRFQMixin.sol";
 
-/// @title 1inch Limit Order Protocol v2
+/**
+ * @title ##1inch Limit Order Protocol v3
+ * @notice Limit order protocol provides two different order types
+ * - Regular Limit Order
+ * - RFQ Order
+ *
+ * Both types provide similar order-fulfilling functionality. The difference is that regular order offers more customization options and features, while RFQ order is extremely gas efficient but without ability to customize.
+ *
+ * Regular limit order additionally supports
+ * - Execution predicates. Conditions for order execution are set with predicates. For example, expiration timestamp or block number, price for stop loss or take profit strategies.
+ * - Callbacks to notify maker on order execution
+ *
+ * See [OrderMixin](OrderMixin.md) for more details.
+ *
+ * RFQ orders supports
+ * - Expiration time
+ * - Cancelation by order id
+ * - Partial Fill (only once)
+ *
+ * See [OrderRFQMixin](OrderRFQMixin.sol) for more details.
+ */
 contract LimitOrderProtocol is
     EIP712("1inch Limit Order Protocol", "3"),
     OrderMixin,
     OrderRFQMixin
 {
+
+    /// @dev Returns the domain separator for the current chain (EIP-712)
     // solhint-disable-next-line func-name-mixedcase
     function DOMAIN_SEPARATOR() external view returns(bytes32) {
         return _domainSeparatorV4();
