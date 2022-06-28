3516673b56b7f283a8c1813c546a06cf4b780a72
Mikhail Melnik <by.zumzoom@gmail.com>
2022-06-28 17:46:47 +0800

    better solcover config

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,8 @@
 module.exports = {
-    skipFiles: [
-        'mocks',
-        'Migrations.sol'
-    ]
+    copyPackages: ['@1inch/solidity-utils', '@openzeppelin/contracts'],
+    skipFiles: ['mocks', 'tests'],
+    mocha: {
+        grep: "@skip-on-coverage",
+        invert: true
+    },
 }
