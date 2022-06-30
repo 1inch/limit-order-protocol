ba7a6ceb1b05bf5c8dab917ce3b3ec538749a0ca
Mikhail Melnik <by.zumzoom@gmail.com>
2022-06-30 21:17:51 +0800

    fix comments about memory safety

--- contracts/libraries/ArgumentsDecoder.sol
+++ contracts/libraries/ArgumentsDecoder.sol
@@ -5,12 +5,11 @@ pragma abicoder v1;
 
 /// @title Library with gas efficient alternatives to `abi.decode`
 library ArgumentsDecoder {
-
     error IncorrectDataLength();
 
     function decodeUint256(bytes calldata data, uint256 offset) internal pure returns(uint256 value) {
         if (data.length < offset + 32) revert IncorrectDataLength();
-        /// @solidity memory-safe-assembly
+        // no memory ops inside so this insertion is automatically memory safe
         assembly { // solhint-disable-line no-inline-assembly
             value := calldataload(add(data.offset, offset))
         }
@@ -18,7 +17,7 @@ library ArgumentsDecoder {
 
     function decodeSelector(bytes calldata data) internal pure returns(bytes4 value) {
         if (data.length < 4) revert IncorrectDataLength();
-        /// @solidity memory-safe-assembly
+        // no memory ops inside so this insertion is automatically memory safe
         assembly { // solhint-disable-line no-inline-assembly
             value := calldataload(data.offset)
         }
@@ -26,7 +25,7 @@ library ArgumentsDecoder {
 
     function decodeTailCalldata(bytes calldata data, uint256 tailOffset) internal pure returns(bytes calldata args) {
         if (data.length < tailOffset) revert IncorrectDataLength();
-        /// @solidity memory-safe-assembly
+        // no memory ops inside so this insertion is automatically memory safe
         assembly {  // solhint-disable-line no-inline-assembly
             args.offset := add(data.offset, tailOffset)
             args.length := sub(data.length, tailOffset)
@@ -35,7 +34,7 @@ library ArgumentsDecoder {
 
     function decodeTargetAndCalldata(bytes calldata data) internal pure returns(address target, bytes calldata args) {
         if (data.length < 20) revert IncorrectDataLength();
-        /// @solidity memory-safe-assembly
+        // no memory ops inside so this insertion is automatically memory safe
         assembly {  // solhint-disable-line no-inline-assembly
             target := shr(96, calldataload(data.offset))
             args.offset := add(data.offset, 20)
