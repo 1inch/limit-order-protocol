757d3f7ee4cc1f47eba75bc4ee7a247ec1853614
Anton Bukov <k06aaa@gmail.com>
2022-07-01 11:34:55 +0200

    Improve comments

--- contracts/OrderRFQLib.sol
+++ contracts/OrderRFQLib.sol
@@ -26,11 +26,11 @@ library OrderRFQLib {
     );
 
     function hash(OrderRFQ memory order, bytes32 domainSeparator) internal pure returns(bytes32 result) {
-        // return keccak256(abi.encode(_LIMIT_ORDER_RFQ_TYPEHASH, order));
         bytes32 typehash = _LIMIT_ORDER_RFQ_TYPEHASH;
         assembly { // solhint-disable-line no-inline-assembly
             let ptr := mload(0x40)
 
+            // keccak256(abi.encode(_LIMIT_ORDER_RFQ_TYPEHASH, order));
             mstore(ptr, typehash)
             mstore(add(ptr, 0x20), mload(order))
             mstore(add(ptr, 0x40), mload(add(order, 0x20)))
@@ -39,11 +39,12 @@ library OrderRFQLib {
             mstore(add(ptr, 0xa0), mload(add(order, 0x80)))
             mstore(add(ptr, 0xc0), mload(add(order, 0xa0)))
             mstore(add(ptr, 0xe0), mload(add(order, 0xc0)))
-            result := keccak256(ptr, 0x100)
+            let orderHash := keccak256(ptr, 0x100)
 
+            // ECDSA.toTypedDataHash(domainSeparator, orderHash)
             mstore(ptr, 0x1901000000000000000000000000000000000000000000000000000000000000) // "\x19\x01"
             mstore(add(ptr, 0x02), domainSeparator)
-            mstore(add(ptr, 0x22), result)
+            mstore(add(ptr, 0x22), orderHash)
             result := keccak256(ptr, 66)
         }
     }
