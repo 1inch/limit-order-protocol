245c6ed8c271254de112d16ac2e36e46529cc6e5
Gleb Alekseev <alekseev.gleb@gmail.com>
2022-07-01 17:44:41 +0300

    upd natspec for IOrderMixin

--- contracts/interfaces/IOrderMixin.sol
+++ contracts/interfaces/IOrderMixin.sol
@@ -5,16 +5,39 @@ pragma solidity 0.8.15;
 import "../OrderLib.sol";
 
 interface IOrderMixin {
-    /// @notice Returns unfilled amount for order. Throws if order does not exist
-    function remaining(bytes32 orderHash) external view returns(uint256);
+    /**
+     * @notice Returns unfilled amount for order. Throws if order does not exist
+     * @param orderHash Order's hash. Can be obtained by the `hashOrder` function
+     * @return amount Unfilled amount
+     */
+    function remaining(bytes32 orderHash) external view returns(uint256 amount);
 
-    /// @notice Returns unfilled amount for order
-    /// @return Result Unfilled amount of order plus one if order exists. Otherwise 0
-    function remainingRaw(bytes32 orderHash) external view returns(uint256);
+    /**
+     * @notice Returns unfilled amount for order
+     * @param orderHash Order's hash. Can be obtained by the `hashOrder` function
+     * @return rawAmount Unfilled amount of order plus one if order exists. Otherwise 0
+     */
+    function remainingRaw(bytes32 orderHash) external view returns(uint256 rawAmount);
 
-    /// @notice Same as `remainingRaw` but for multiple orders
-    function remainingsRaw(bytes32[] memory orderHashes) external view returns(uint256[] memory);
-    function checkPredicate(OrderLib.Order calldata order) external view returns(bool);
+    /**
+     * @notice Same as `remainingRaw` but for multiple orders
+     * @param orderHashes Array of hashes
+     * @return rawAmounts Array of amounts for each order plus one if order exists or 0 otherwise
+     */
+    function remainingsRaw(bytes32[] memory orderHashes) external view returns(uint256[] memory rawAmounts);
+
+    /**
+     * @notice Checks order predicate
+     * @param order Order to check predicate for
+     * @return result Predicate evaluation result. True if predicate allows to fill the order, false otherwise
+     */
+    function checkPredicate(OrderLib.Order calldata order) external view returns(bool result);
+
+    /**
+     * @notice Returns order hash according to EIP712 standart
+     * @param order Order to get hash for
+     * @return orderHash Hash of the order
+     */
     function hashOrder(OrderLib.Order calldata order) external view returns(bytes32);
 
     /**
@@ -24,15 +47,26 @@ interface IOrderMixin {
      */
     function simulate(address target, bytes calldata data) external;
 
-    /// @notice Cancels order by setting remaining amount to zero
+    /**
+     * @notice Cancels order.
+     * @dev Order is cancelled by setting remaining amount to _ORDER_FILLED value
+     * @param order Order quote to cancel
+     * @return orderRemaining Unfilled amount of order before cancellation
+     * @return orderHash Hash of the filled order
+     */
     function cancelOrder(OrderLib.Order calldata order) external returns(uint256 orderRemaining, bytes32 orderHash);
 
-    /// @notice Fills an order. If one doesn't exist (first fill) it will be created using order.makerAssetData
-    /// @param order Order quote to fill
-    /// @param signature Signature to confirm quote ownership
-    /// @param makingAmount Making amount
-    /// @param takingAmount Taking amount
-    /// @param thresholdAmount Specifies maximum allowed takingAmount when takingAmount is zero, otherwise specifies minimum allowed makingAmount
+    /**
+     * @notice Fills an order. If one doesn't exist (first fill) it will be created using order.makerAssetData
+     * @param order Order quote to fill
+     * @param signature Signature to confirm quote ownership
+     * @param makingAmount Making amount
+     * @param takingAmount Taking amount
+     * @param thresholdAmount Specifies maximum allowed takingAmount when takingAmount is zero, otherwise specifies minimum allowed makingAmount
+     * @return actualMakingAmount Actual amount transferred from maker to taker
+     * @return actualTakingAmount Actual amount transferred from taker to maker
+     * @return orderHash Hash of the filled order
+     */
     function fillOrder(
         OrderLib.Order calldata order,
         bytes calldata signature,
@@ -40,19 +74,24 @@ interface IOrderMixin {
         uint256 makingAmount,
         uint256 takingAmount,
         uint256 thresholdAmount
-    ) external returns(uint256 /* actualMakingAmount */, uint256 /* actualTakingAmount */, bytes32 orderHash);
+    ) external returns(uint256 actualMakingAmount, uint256 actualTakingAmount, bytes32 orderHash);
 
-    /// @notice Same as `fillOrder` but calls permit first,
-    /// allowing to approve token spending and make a swap in one transaction.
-    /// Also allows to specify funds destination instead of `msg.sender`
-    /// @param order Order quote to fill
-    /// @param signature Signature to confirm quote ownership
-    /// @param makingAmount Making amount
-    /// @param takingAmount Taking amount
-    /// @param thresholdAmount Specifies maximum allowed takingAmount when takingAmount is zero, otherwise specifies minimum allowed makingAmount
-    /// @param target Address that will receive swap funds
-    /// @param permit Should consist of abiencoded token address and encoded `IERC20Permit.permit` call.
-    /// @dev See tests for examples
+    /**
+     * @notice Same as `fillOrder` but calls permit first,
+     * allowing to approve token spending and make a swap in one transaction.
+     * Also allows to specify funds destination instead of `msg.sender`
+     * @dev See tests for examples
+     * @param order Order quote to fill
+     * @param signature Signature to confirm quote ownership
+     * @param makingAmount Making amount
+     * @param takingAmount Taking amount
+     * @param thresholdAmount Specifies maximum allowed takingAmount when takingAmount is zero, otherwise specifies minimum allowed makingAmount
+     * @param target Address that will receive swap funds
+     * @param permit Should consist of abiencoded token address and encoded `IERC20Permit.permit` call.
+     * @return actualMakingAmount Actual amount transferred from maker to taker
+     * @return actualTakingAmount Actual amount transferred from taker to maker
+     * @return orderHash Hash of the filled order
+     */
     function fillOrderToWithPermit(
         OrderLib.Order calldata order,
         bytes calldata signature,
@@ -62,15 +101,20 @@ interface IOrderMixin {
         uint256 thresholdAmount,
         address target,
         bytes calldata permit
-    ) external returns(uint256 /* actualMakingAmount */, uint256 /* actualTakingAmount */, bytes32 orderHash);
-
-    /// @notice Same as `fillOrder` but allows to specify funds destination instead of `msg.sender`
-    /// @param order_ Order quote to fill
-    /// @param signature Signature to confirm quote ownership
-    /// @param makingAmount Making amount
-    /// @param takingAmount Taking amount
-    /// @param thresholdAmount Specifies maximum allowed takingAmount when takingAmount is zero, otherwise specifies minimum allowed makingAmount
-    /// @param target Address that will receive swap funds
+    ) external returns(uint256 actualMakingAmount, uint256 actualTakingAmount, bytes32 orderHash);
+    
+    /**
+     * @notice Same as `fillOrder` but allows to specify funds destination instead of `msg.sender`
+     * @param order_ Order quote to fill
+     * @param signature Signature to confirm quote ownership
+     * @param makingAmount Making amount
+     * @param takingAmount Taking amount
+     * @param thresholdAmount Specifies maximum allowed takingAmount when takingAmount is zero, otherwise specifies minimum allowed makingAmount
+     * @param target Address that will receive swap funds
+     * @return actualMakingAmount Actual amount transferred from maker to taker
+     * @return actualTakingAmount Actual amount transferred from taker to maker
+     * @return orderHash Hash of the filled order
+     */
     function fillOrderTo(
         OrderLib.Order calldata order_,
         bytes calldata signature,
@@ -79,5 +123,5 @@ interface IOrderMixin {
         uint256 takingAmount,
         uint256 thresholdAmount,
         address target
-    ) external returns(uint256 /* actualMakingAmount */, uint256 /* actualTakingAmount */, bytes32 orderHash);
+    ) external returns(uint256 actualMakingAmount, uint256 actualTakingAmount, bytes32 orderHash);
 }
