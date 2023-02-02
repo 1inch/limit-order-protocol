92ff0274a184860174d33ba76cf2bef4d0b0fb92
SevenSwen <tihonenko.v.v@gmail.com>
2023-02-02 21:08:19 +0700

    update RecursiveMatcher for matching RFQ orders

--- contracts/mocks/RecursiveMatcher.sol
+++ contracts/mocks/RecursiveMatcher.sol
@@ -5,9 +5,11 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IInteractionNotificationReceiver.sol";
 import "../interfaces/IOrderMixin.sol";
+import "../interfaces/IOrderRFQMixin.sol";
 
 contract RecursiveMatcher is IInteractionNotificationReceiver {
     bytes1 private constant _FINALIZE_INTERACTION = 0x01;
+    bytes1 private constant _RFQ_FLAG = 0x02; // set this flag, if RFQ order is filling
 
     error IncorrectCalldataParams();
     error FailedExternalCall();
@@ -31,13 +33,28 @@ contract RecursiveMatcher is IInteractionNotificationReceiver {
         );
     }
 
+    function matchRfqOrders(
+        IOrderRFQMixin orderRFQMixin,
+        OrderRFQLib.OrderRFQ calldata order,
+        bytes calldata signature,
+        bytes calldata interaction,
+        uint256 flagsAndAmount
+    ) external {
+        orderRFQMixin.fillOrderRFQ(
+            order,
+            signature,
+            interaction,
+            flagsAndAmount
+        );
+    }
+
     function fillOrderInteraction(
         address /* taker */,
         uint256 /* makingAmount */,
         uint256 /* takingAmount */,
         bytes calldata interactiveData
     ) external returns(uint256) {
-        if(interactiveData[0] == _FINALIZE_INTERACTION) {
+        if(interactiveData[0] & _FINALIZE_INTERACTION != 0x0) {
             (
                 address[] memory targets,
                 bytes[] memory calldatas
@@ -50,23 +67,39 @@ contract RecursiveMatcher is IInteractionNotificationReceiver {
                 if(!success) revert FailedExternalCall();
             }
         } else {
-            (
-                OrderLib.Order memory order,
-                bytes memory signature,
-                bytes memory interaction,
-                uint256 makingOrderAmount,
-                uint256 takingOrderAmount,
-                uint256 thresholdAmount
-            ) = abi.decode(interactiveData[1:], (OrderLib.Order, bytes, bytes, uint256, uint256, uint256));
+            if(interactiveData[0] & _RFQ_FLAG != 0x0) {
+                (
+                    OrderRFQLib.OrderRFQ memory order,
+                    bytes memory signature,
+                    bytes memory interaction,
+                    uint256 flagsAndAmount
+                ) = abi.decode(interactiveData[1:], (OrderRFQLib.OrderRFQ, bytes, bytes, uint256));
 
-            IOrderMixin(msg.sender).fillOrder(
-                order,
-                signature,
-                interaction,
-                makingOrderAmount,
-                takingOrderAmount,
-                thresholdAmount
-            );
+                IOrderRFQMixin(msg.sender).fillOrderRFQ(
+                    order,
+                    signature,
+                    interaction,
+                    flagsAndAmount
+                );
+            } else {
+                (
+                    OrderLib.Order memory order,
+                    bytes memory signature,
+                    bytes memory interaction,
+                    uint256 makingOrderAmount,
+                    uint256 takingOrderAmount,
+                    uint256 thresholdAmount
+                ) = abi.decode(interactiveData[1:], (OrderLib.Order, bytes, bytes, uint256, uint256, uint256));
+
+                IOrderMixin(msg.sender).fillOrder(
+                    order,
+                    signature,
+                    interaction,
+                    makingOrderAmount,
+                    takingOrderAmount,
+                    thresholdAmount
+                );
+            }
         }
         return 0;
     }
