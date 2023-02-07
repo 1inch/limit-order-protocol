272c7b62e705f21175f18979568bd2ca28f35c63
Anton Bukov <k06aaa@gmail.com>
2023-02-07 23:05:53 +0100

    Unchecked calculator

--- contracts/helpers/AmountCalculator.sol
+++ contracts/helpers/AmountCalculator.sol
@@ -7,12 +7,22 @@ library AmountCalculator {
     /// @notice Calculates maker amount
     /// @return Result Floored maker amount
     function getMakingAmount(uint256 orderMakerAmount, uint256 orderTakerAmount, uint256 swapTakerAmount) internal pure returns(uint256) {
+        if ((swapTakerAmount | orderMakerAmount) >> 128 == 0) {
+            unchecked {
+                return (swapTakerAmount * orderMakerAmount) / orderTakerAmount;
+            }
+        }
         return swapTakerAmount * orderMakerAmount / orderTakerAmount;
     }
 
     /// @notice Calculates taker amount
     /// @return Result Ceiled taker amount
     function getTakingAmount(uint256 orderMakerAmount, uint256 orderTakerAmount, uint256 swapMakerAmount) internal pure returns(uint256) {
+        if ((swapMakerAmount | orderTakerAmount) >> 128 == 0) {
+            unchecked {
+                return (swapMakerAmount * orderTakerAmount + orderMakerAmount - 1) / orderMakerAmount;
+            }
+        }
         return (swapMakerAmount * orderTakerAmount + orderMakerAmount - 1) / orderMakerAmount;
     }
 }
