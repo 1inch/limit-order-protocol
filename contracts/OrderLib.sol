445eb47d04fb9abb6c9ec608c7c96b919670b344
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-06-13 14:51:46 +0300

    wip

--- contracts/OrderLib.sol
+++ contracts/OrderLib.sol
@@ -10,22 +10,27 @@ import "./libraries/MakerTraitsLib.sol";
 import "./libraries/ExtensionLib.sol";
 import "./helpers/AmountCalculator.sol";
 
-/// @title OrderUtils
-/// @dev Library for common order functions.
-library OrderLib {
+/**
+ * @title OrderLib
+ * @dev The library provides common functionality for processing and manipulating limit orders. 
+ * It provides functionality to calculate and verify order hashes, calculate trade amounts, and validate 
+ * extension data associated with orders. The library also contains helper methods to get the receiver of 
+ * an order and call getter functions.
+ */
+ library OrderLib {
     using AddressLib for Address;
     using MakerTraitsLib for MakerTraits;
     using ExtensionLib for bytes;
 
-    /// @dev Error for incorrect getter function called.
+    /// @dev Error to be thrown when the incorrect getter function called.
     error WrongGetter();
-    /// @dev Error when the amount call fails.
+    /// @dev Error to be thrown when the call to get the amount fails.
     error GetAmountCallFailed();
-    /// @dev Error for missing order extension.
+    /// @dev Error to be thrown when the extension data of an order is missing.
     error MissingOrderExtension();
-    /// @dev Error for unexpected order extension.
+    /// @dev Error to be thrown when the order has an unexpected extension.
     error UnexpectedOrderExtension();
-    /// @dev Error for invalid extension.
+    /// @dev rror to be thrown when the order extension is invalid.
     error ExtensionInvalid();
 
     /// @dev The typehash of the order struct.
@@ -45,10 +50,10 @@ library OrderLib {
     uint256 constant internal _DATA_HASH_SIZE = 0x120;
 
     /**
-      * @dev Calculates the hash of an order.
-      * @param order The order to hash.
-      * @param domainSeparator The EIP-712 domain separator to use.
-      * @return result The hash of the order.
+      * @notice Calculates the hash of an order.
+      * @param order The order to be hashed.
+      * @param domainSeparator The domain separator to be used for the EIP-712 hashing.
+      * @return The keccak256 hash of the order data.
       */
     function hash(IOrderMixin.Order calldata order, bytes32 domainSeparator) internal pure returns(bytes32 result) {
         bytes32 typehash = _LIMIT_ORDER_TYPEHASH;
@@ -64,19 +69,22 @@ library OrderLib {
     }
 
     /**
-      * @dev Gets the receiver address of an order.
+      * @notice Returns the receiver address for an order.
       * @param order The order.
-      * @return receiver The receiver address.
+      * @return receiver The address of the receiver, either explicitly defined in the order or the maker's address if not specified.
       */
-    function getReceiver(IOrderMixin.Order calldata order) internal pure returns(address) {
+    function getReceiver(IOrderMixin.Order calldata order) internal pure returns(address /*receiver*/) {
         address receiver = order.receiver.get();
         return receiver != address(0) ? receiver : order.maker.get();
     }
 
-    /** @dev Calculates the amount of the asset the maker receives in exchange for the asset they provide.
+    /** 
+      * @notice Calculates the making amount based on the requested taking amount.
+      * @dev If getter is specified in the extension data, the getter is called to calculate the making amount,
+      * otherwise the making amount is calculated linearly.
       * @param order The order.
       * @param extension The extension data associated with the order.
-      * @param requestedTakingAmount The amount of the asset the taker wants to take.
+      * @param requestedTakingAmount The amount the taker wants to take.
       * @param remainingMakingAmount The remaining amount of the asset left to fill.
       * @param orderHash The hash of the order.
       * @return makingAmount The amount of the asset the maker receives.
@@ -96,12 +104,14 @@ library OrderLib {
         return _callGetter(getter, requestedTakingAmount, remainingMakingAmount, orderHash);
     }
 
-
     /**
+      * @notice Calculates the taking amount based on the requested making amount.
+      * @dev If getter is specified in the extension data, the getter is called to calculate the taking amount,
+      * otherwise the taking amount is calculated linearly.
       * @param order The order.
       * @param extension The extension data associated with the order.
-      * @param requestedMakingAmount The amount of the asset the maker wants to receive.
-      * @param remainingMakingAmount The remaining amount of the asset left to fill.
+      * @param requestedMakingAmount The amount the maker wants to receive.
+      * @param remainingMakingAmount The remaining amount of the asset left to be filled.
       * @param orderHash The hash of the order.
       * @return takingAmount The amount of the asset the taker takes.
       */
@@ -121,7 +131,7 @@ library OrderLib {
     }
 
     /**
-      * @dev Internal function that calls a getter function to calculate an amount for a trade.
+      * @notice Calls the getter function to calculate an amount for a trade.
       * @param getter The address of the getter function.
       * @param requestedAmount The amount requested by the taker.
       * @param remainingMakingAmount The remaining amount of the asset left to fill.
--- contracts/libraries/MakerTraitsLib.sol
+++ contracts/libraries/MakerTraitsLib.sol
@@ -4,6 +4,28 @@ pragma solidity ^0.8.0;
 
 type MakerTraits is uint256;
 
+/**
+ * @title MakerTraitsLib
+ * @notice A library to manage and check MakerTraits, which are used to encode the maker's preferences for an order in a single uint256.
+ * @dev
+ * The MakerTraits type is a uint256 and different parts of the number are used to encode different traits.
+ * High bits are used for flags
+ * 255 bit `NO_PARTIAL_FILLS_FLAG`          - if set, the order does not allow partial fills
+ * 254 bit `ALLOW_MULTIPLE_FILLS_FLAG`      - if set, the order permits multiple fills
+ * 253 bit `NO_IMPROVE_RATE_FLAG`           - if set, the order does not allow taker interaction to improve rate
+ * 252 bit `PRE_INTERACTION_CALL_FLAG`      - if set, the order requires pre-interaction call
+ * 251 bit `POST_INTERACTION_CALL_FLAG`     - if set, the order requires post-interaction call
+ * 250 bit `NEED_CHECK_EPOCH_MANAGER_FLAG`  - if set, the order requires to check the epoch manager
+ * 249 bit `HAS_EXTENSION_FLAG`             - if set, the order has extension(s)
+ * 248 bit `USE_PERMIT2_FLAG`               - if set, the order uses permit2
+ * 247 bit `UNWRAP_WETH_FLAG`               - if set, the order requires to unwrap WETH
+
+ * Low 200 bits are used for allowed sender, expiration, nonceOrEpoch, and series
+ * uint80 last 10 bytes of allowed sender address (0 if any)
+ * uint40 expiration timestamp (0 if none)
+ * uint40 nonce or epoch
+ * uint40 series
+ */
 library MakerTraitsLib {
     // Low 200 bits are used for allowed sender, expiration, nonceOrEpoch, and series
     uint256 private constant _ALLOWED_SENDER_MASK = type(uint80).max;
@@ -24,60 +46,136 @@ library MakerTraitsLib {
     uint256 private constant _USE_PERMIT2_FLAG = 1 << 248;
     uint256 private constant _UNWRAP_WETH_FLAG = 1 << 247;
 
+    /**
+     * @notice Checks if the order has the extension flag set.
+     * @dev If the `HAS_EXTENSION_FLAG` is set in the makerTraits, then the protocol expects that the order has extension(s).
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the flag is set.
+     */
     function hasExtension(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _HAS_EXTENSION_FLAG) != 0;
     }
 
+    /**
+     * @notice Checks if the maker allows a specific taker to fill the order.
+     * @param makerTraits The traits of the maker.
+     * @param sender The address of the taker to be checked.
+     * @return result A boolean indicating whether the taker is allowed.
+     */
     function isAllowedSender(MakerTraits makerTraits, address sender) internal pure returns (bool) {
         uint160 allowedSender = uint160(MakerTraits.unwrap(makerTraits) & _ALLOWED_SENDER_MASK);
         return allowedSender == 0 || allowedSender == uint160(sender) & _ALLOWED_SENDER_MASK;
     }
 
+    /**
+     * @notice Checks if the order has expired.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the order has expired.
+     */
     function isExpired(MakerTraits makerTraits) internal view returns (bool) {
         uint256 expiration = (MakerTraits.unwrap(makerTraits) >> _EXPIRATION_OFFSET) & _EXPIRATION_MASK;
         return expiration != 0 && expiration < block.timestamp;  // solhint-disable-line not-rely-on-time
     }
 
+    /**
+     * @notice Returns the nonce or epoch of the order.
+     * @param makerTraits The traits of the maker.
+     * @return result The nonce or epoch of the order.
+     */
     function nonceOrEpoch(MakerTraits makerTraits) internal pure returns (uint256) {
         return (MakerTraits.unwrap(makerTraits) >> _NONCE_OR_EPOCH_OFFSET) & _NONCE_OR_EPOCH_MASK;
     }
 
+    /**
+     * @notice Returns the series of the order.
+     * @param makerTraits The traits of the maker.
+     * @return result The series of the order.
+     */
     function series(MakerTraits makerTraits) internal pure returns (uint256) {
         return (MakerTraits.unwrap(makerTraits) >> _SERIES_OFFSET) & _SERIES_MASK;
     }
 
+    /**
+      * @notice Determines if the order allows partial fills.
+      * @dev If the _NO_PARTIAL_FILLS_FLAG is not set in the makerTraits, then the order allows partial fills.
+      * @param makerTraits The traits of the maker, determining their preferences for the order.
+      * @return result A boolean indicating whether the maker allows partial fills.
+      */
     function allowPartialFills(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _NO_PARTIAL_FILLS_FLAG) == 0;
     }
 
+    /**
+     * @notice Checks if the order allows taker interaction to improve rate.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the order allows to improve rate.
+     */
     function allowImproveRateViaInteraction(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _NO_IMPROVE_RATE_FLAG) == 0;
     }
 
+    /**
+     * @notice Checks if the maker needs pre-interaction call.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the maker needs a pre-interaction call.
+     */
     function needPreInteractionCall(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _PRE_INTERACTION_CALL_FLAG) != 0;
     }
 
+    /**
+     * @notice Checks if the maker needs post-interaction call.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the maker needs a post-interaction call.
+     */
     function needPostInteractionCall(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _POST_INTERACTION_CALL_FLAG) != 0;
     }
 
+    /**
+      * @notice Determines if the order allows multiple fills.
+      * @dev If the _ALLOW_MULTIPLE_FILLS_FLAG is set in the makerTraits, then the maker allows multiple fills.
+      * @param makerTraits The traits of the maker, determining their preferences for the order.
+      * @return result A boolean indicating whether the maker allows multiple fills.
+      */
     function allowMultipleFills(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _ALLOW_MULTIPLE_FILLS_FLAG) != 0;
     }
 
+    /**
+      * @notice Determines if an order should use the bit invalidator or remaining amount validator.
+      * @dev The bit invalidator can be used if the order does not allow partial or multiple fills.
+      * @param makerTraits The traits of the maker, determining their preferences for the order.
+      * @return result A boolean indicating whether the bit invalidator should be used. 
+      * True if the order requires the use of the bit invalidator.
+      */
     function useBitInvalidator(MakerTraits makerTraits) internal pure returns (bool) {
         return !allowPartialFills(makerTraits) || !allowMultipleFills(makerTraits);
     }
 
+    /**
+     * @notice Checks if the maker needs to check the epoch.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the maker needs to check the epoch manager.
+     */
     function needCheckEpochManager(MakerTraits makerTraits) internal pure returns (bool) {
         return (MakerTraits.unwrap(makerTraits) & _NEED_CHECK_EPOCH_MANAGER_FLAG) != 0;
     }
 
+    /**
+     * @notice Checks if the maker uses permit2.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the maker uses permit2.
+     */
     function usePermit2(MakerTraits makerTraits) internal pure returns (bool) {
         return MakerTraits.unwrap(makerTraits) & _USE_PERMIT2_FLAG != 0;
     }
 
+    /**
+     * @notice Checks if the maker needs to unwraps WETH.
+     * @param makerTraits The traits of the maker.
+     * @return result A boolean indicating whether the maker needs to unwrap WETH.
+     */
     function unwrapWeth(MakerTraits makerTraits) internal pure returns (bool) {
         return MakerTraits.unwrap(makerTraits) & _UNWRAP_WETH_FLAG != 0;
     }
