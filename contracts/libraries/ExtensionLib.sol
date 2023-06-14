c65589720c4e778ffecfa750587738b74e7a483d
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-06-15 02:19:45 +0300

    wip extensionLib

--- contracts/libraries/ExtensionLib.sol
+++ contracts/libraries/ExtensionLib.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.0;
 import "../interfaces/IOrderMixin.sol";
 import "./OffsetsLib.sol";
 
+/**
+ * @title ExtensionLib
+ * @notice Library for retrieving extensions information for the IOrderMixin Interface.
+ */
 library ExtensionLib {
     using AddressLib for Address;
     using OffsetsLib for Offsets;
@@ -20,38 +24,86 @@ library ExtensionLib {
         PostInteractionData
     }
 
+    /**
+     * @notice Returns the MakerAssetSuffix from the provided extension calldata.
+     * @param extension The calldata from which the MakerAssetSuffix is to be retrieved.
+     * @return calldata Bytes representing the MakerAssetSuffix.
+     */
     function makerAssetSuffix(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.MakerAssetSuffix);
     }
 
+    /**
+     * @notice Returns the TakerAssetSuffix from the provided extension calldata.
+     * @param extension The calldata from which the TakerAssetSuffix is to be retrieved.
+     * @return calldata Bytes representing the TakerAssetSuffix.
+     */
     function takerAssetSuffix(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.TakerAssetSuffix);
     }
 
+    /**
+     * @notice Returns the MakingAmountGetter from the provided extension calldata.
+     * @param extension The calldata from which the MakingAmountGetter is to be retrieved.
+     * @return calldata Bytes representing the MakingAmountGetter.
+     */
     function makingAmountGetter(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.MakingAmountGetter);
     }
 
+    /**
+     * @notice Returns the TakingAmountGetter from the provided extension calldata.
+     * @param extension The calldata from which the TakingAmountGetter is to be retrieved.
+     * @return calldata Bytes representing the TakingAmountGetter.
+     */
     function takingAmountGetter(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.TakingAmountGetter);
     }
 
+    /**
+     * @notice Returns the order's predicate from the provided extension calldata.
+     * @param extension The calldata from which the predicate is to be retrieved.
+     * @return calldata Bytes representing the predicate.
+     */
     function predicate(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.Predicate);
     }
 
+    /**
+     * @notice Returns the maker's permit from the provided extension calldata.
+     * @param extension The calldata from which the maker's permit is to be retrieved.
+     * @return calldata Bytes representing the maker's permit.
+     */
     function makerPermit(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.MakerPermit);
     }
 
+
+    /**
+     * @notice Returns the pre-interaction from the provided extension calldata.
+     * @param extension The calldata from which the pre-interaction is to be retrieved.
+     * @return calldata Bytes representing the pre-interaction.
+     */
     function preInteractionTargetAndData(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.PreInteractionData);
     }
 
+    /**
+     * @notice Returns the post-interaction from the provided extension calldata.
+     * @param extension The calldata from which the post-interaction is to be retrieved.
+     * @return calldata Bytes representing the post-interaction.
+     */
     function postInteractionTargetAndData(bytes calldata extension) internal pure returns(bytes calldata) {
         return _get(extension, DynamicField.PostInteractionData);
     }
 
+    /**
+     * @notice Retrieves a specific field from the provided extension calldata.
+     * @dev The first 32 bytes of an extension calldata contain offsets to the end of each field within the calldata.
+     * @param extension The calldata from which the field is to be retrieved.
+     * @param field The specific dynamic field to retrieve from the extension.
+     * @return result A bytes calldata representing the requested field.
+     */
     function _get(bytes calldata extension, DynamicField field) private pure returns(bytes calldata result) {
         if (extension.length < 0x20) return msg.data[:0];
 
