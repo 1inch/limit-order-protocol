b4a688bc8b8461c01cc442a60d444fd412405aaf
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-06-15 13:09:01 +0300

    invalidator docs

--- contracts/libraries/BitInvalidatorLib.sol
+++ contracts/libraries/BitInvalidatorLib.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.0;
 
 /**
  * @title BitInvalidatorLib
- * @dev This library provides functionality to invalidate objects based on a bit invalidator.
- * The Data struct holds a mapping where each key represents a slot number and each value contains an integer.
- * Each bit of the integer represents whether the corresponding object is still valid or has been invalidated (0 - valid, 1 - invalidated).
- * To access an object's state or invalidate it, a nonce must be passed that identifies it, and follows the following rules:
+ * @dev The library provides a mechanism to invalidate objects based on a bit invalidator.
+ * The bit invalidator holds a mapping where each key represents a slot number and each value contains an integer.
+ * Each bit of the integer represents whether the object with corresponding index is valid or has been invalidated (0 - valid, 1 - invalidated).
+ * The nonce given to access or invalidate an entity's state follows this structure:
  * - bits [0..7] represent the object state index in the slot.
  * - bits [8..255] represent the slot number (mapping key).
  */
 library BitInvalidatorLib {
-    /// @dev Error thrown when a bit has already been invalidated for an order.
+    /// @dev The error is thrown when an attempt is made to invalidate an already invalidated entity.
     error BitInvalidatedOrder();
 
     struct Data {
@@ -20,10 +20,11 @@ library BitInvalidatorLib {
     }
 
     /**
-     * @dev Returns the values for a specific slot.
+     * @notice Retrieves the validity status of entities in a specific slot.
+     * @dev Each bit in the returned value corresponds to the validity of an entity. 0 for valid, 1 for invalidated.
      * @param self The data structure.
-     * @param nonce The nonce specifing the slot.
-     * @return uint256 The value of the specific slot.
+     * @param nonce The nonce identifying the slot.
+     * @return result The validity status of entities in the slot as a uint256.
      */
     function checkSlot(Data storage self, uint256 nonce) internal view returns(uint256) {
         uint256 invalidatorSlot = nonce >> 8;
@@ -31,9 +32,10 @@ library BitInvalidatorLib {
     }
 
     /**
-     * @dev Invalidates the specific bit in the specific slot.
+     * @notice Checks the validity of a specific entity and invalidates it if valid.
+     * @dev Throws an error if the entity has already been invalidated.
      * @param self The data structure.
-     * @param nonce The nonce specifing the slot and the object index.
+     * @param nonce The nonce identifying the slot and the entity.
      */
     function checkAndInvalidate(Data storage self, uint256 nonce) internal {
         uint256 invalidatorSlot = nonce >> 8;
@@ -44,9 +46,10 @@ library BitInvalidatorLib {
     }
 
     /**
-     * @dev Invalidates multiple objects in a single slot.
+     * @notice Invalidates multiple entities in a single slot.
+     * @dev The entities to be invalidated are identified by setting their corresponding bits to 1 in a mask.
      * @param self The data structure.
-     * @param nonce The nonce specifing the slot.
+     * @param nonce The nonce identifying the slot.
      * @param additionalMask A mask of bits to be invalidated.
      */
     function massInvalidate(Data storage self, uint256 nonce, uint256 additionalMask) internal {
