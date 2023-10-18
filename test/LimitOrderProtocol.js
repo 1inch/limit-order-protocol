6c31ee949ae067b27eb7038e0b610b4b783f4cce
Mikhail Melnik <by.zumzoom@gmail.com>
2023-10-18 17:16:09 +0200

    linter

--- test/LimitOrderProtocol.js
+++ test/LimitOrderProtocol.js
@@ -1,6 +1,6 @@
 const hre = require('hardhat');
 const { ethers, tracer } = hre;
-const { expect, time, constants, getPermit2, permit2Contract } = require('@1inch/solidity-utils');
+const { expect, time, constants } = require('@1inch/solidity-utils');
 const { fillWithMakingAmount, unwrapWethTaker, buildMakerTraits, buildOrder, signOrder, buildOrderData, buildTakerTraits } = require('./helpers/orderUtils');
 const { getPermit, withTarget } = require('./helpers/eip712');
 const { joinStaticCalls, ether, findTrace, countAllItems } = require('./helpers/utils');
