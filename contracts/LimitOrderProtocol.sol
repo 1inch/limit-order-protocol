3169ea46932ef44114a215a60d1d91ef022b416d
Mikhail Melnik <by.zumzoom@gmail.com>
2024-01-31 18:25:10 +0400

    add pausable

--- contracts/LimitOrderProtocol.sol
+++ contracts/LimitOrderProtocol.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.23;
 
 import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "./OrderMixin.sol";
 
 /**
@@ -28,14 +30,30 @@ import "./OrderMixin.sol";
  */
 contract LimitOrderProtocol is
     EIP712("1inch Limit Order Protocol", "4"),
+    Ownable,
+    Pausable,
     OrderMixin
 {
     // solhint-disable-next-line no-empty-blocks
-    constructor(IWETH _weth) OrderMixin(_weth) {}
+    constructor(IWETH _weth) OrderMixin(_weth) Ownable(msg.sender) {}
 
     /// @dev Returns the domain separator for the current chain (EIP-712)
     // solhint-disable-next-line func-name-mixedcase
     function DOMAIN_SEPARATOR() external view returns(bytes32) {
         return _domainSeparatorV4();
+    }
+
+    /**
+     * @notice Pauses all the trading functionality in the contract.
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @notice Unpauses all the trading functionality in the contract.
+     */
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
