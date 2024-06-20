e63795b3b7fd98b135f47dbf3e5861264ae3e4a8
Mikhail Melnik <by.zumzoom@gmail.com>
2024-06-20 13:09:09 +0800

    create OrderRegistrator

---
+++ contracts/helpers/OrderRegistrator.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import { Address, AddressLib } from "@1inch/solidity-utils/contracts/libraries/AddressLib.sol";
+import { ECDSA } from "@1inch/solidity-utils/contracts/libraries/ECDSA.sol";
+import { IOrderMixin } from "../interfaces/IOrderMixin.sol";
+import { OrderLib } from "../OrderLib.sol";
+
+contract OrderRegistrator {
+    using AddressLib for Address;
+    using OrderLib for IOrderMixin.Order;
+
+    error BadSignature();
+
+    event OrderRegistered(IOrderMixin.Order order, bytes extension, bytes signature);
+
+    IOrderMixin private immutable _LIMIT_ORDER_PROTOCOL;
+
+    constructor(IOrderMixin limitOrderProtocol) {
+        _LIMIT_ORDER_PROTOCOL = limitOrderProtocol;
+    }
+
+    function registerOrder(IOrderMixin.Order calldata order, bytes calldata extension, bytes calldata signature) external {
+        // Validate order
+        {
+            (bool valid, bytes4 validationResult) = order.isValidExtension(extension);
+            if (!valid) {
+                // solhint-disable-next-line no-inline-assembly
+                assembly ("memory-safe") {
+                    mstore(0, validationResult)
+                    revert(0, 4)
+                }
+            }
+        }
+
+        // Validate signature
+        if(!ECDSA.recoverOrIsValidSignature(order.maker.get(), _LIMIT_ORDER_PROTOCOL.hashOrder(order), signature)) revert BadSignature();
+
+        emit OrderRegistered(order, extension, signature);
+    }
+}
