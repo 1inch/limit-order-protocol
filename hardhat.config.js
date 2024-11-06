9eb209d7d21a90f82db4c15edcf3577b073266f3
Denis <zzomrot.ne@gmail.com>
2024-11-06 23:19:28 +0400

    Update solidity-utils and docify script

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,13 @@ require('@matterlabs/hardhat-zksync-deploy');
 require('@matterlabs/hardhat-zksync-solc');
 require('@nomicfoundation/hardhat-chai-matchers');
 require('solidity-coverage');
+require('solidity-docgen');
 require('hardhat-dependency-compiler');
 require('hardhat-deploy');
 require('hardhat-gas-reporter');
 require('hardhat-tracer');
 require('dotenv').config();
+const { oneInchTemplates } = require('@1inch/solidity-utils/docgen');
 const { Networks, getNetwork } = require('@1inch/solidity-utils/hardhat-setup');
 
 if (getNetwork().indexOf('zksync') !== -1) {
@@ -54,5 +56,11 @@ module.exports = {
         version: '1.4.0',
         compilerSource: 'binary',
         settings: {},
+    },
+    docgen: {
+        outputDir: 'docs',
+        templates: oneInchTemplates(),
+        pages: 'files',
+        exclude: ['mocks'],
     },
 };
