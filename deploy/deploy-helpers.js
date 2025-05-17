f73ab083d6932765ac1c840d7ba3ca55870d8335
Denis <zzomrot.ne@gmail.com>
2025-05-17 10:23:38 +0100

    Patch helpers deploy script

--- deploy/deploy-helpers.js
+++ deploy/deploy-helpers.js
@@ -1,49 +1,55 @@
+const { deployAndGetContract } = require('@1inch/solidity-utils');
 const hre = require('hardhat');
 const { getChainId } = hre;
+
+const LOP = '0x111111125421cA6dc452d289314280a0f8842A65'; // All chains
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running deploy script');
     const chainId = await getChainId();
     console.log('network id ', chainId);
 
-    const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const seriesNonceManager = await deploy('SeriesNonceManager', {
-        from: deployer,
+    // SeriesNonceManager
+    await deployAndGetContract({
+        contractName: 'SeriesNonceManager',
+        constructorArgs: [],
+        deployments,
+        deployer,
     });
 
-    console.log('SeriesNonceManager deployed to:', seriesNonceManager.address);
-
-    if (await getChainId() !== '31337') {
-        await hre.run('verify:verify', {
-            address: seriesNonceManager.address,
-        });
-    }
-
-    const callsSimulator = await deploy('CallsSimulator', {
-        from: deployer,
+    // CallsSimulator
+    await deployAndGetContract({
+        contractName: 'CallsSimulator',
+        constructorArgs: [],
+        deployments,
+        deployer,
     });
 
-    console.log('CallsSimulator deployed to:', callsSimulator.address);
-
-    if (await getChainId() !== '31337') {
-        await hre.run('verify:verify', {
-            address: callsSimulator.address,
-        });
-    }
-
-    const priorityFeeLimiter = await deploy('PriorityFeeLimiter', {
-        from: deployer,
+    // PriorityFeeLimiter
+    await deployAndGetContract({
+        contractName: 'PriorityFeeLimiter',
+        constructorArgs: [],
+        deployments,
+        deployer,
     });
 
-    console.log('PriorityFeeLimiter deployed to:', priorityFeeLimiter.address);
+    // OrderRegistrator
+    const orderRegistrator = await deployAndGetContract({
+        contractName: 'OrderRegistrator',
+        constructorArgs: [LOP],
+        deployments,
+        deployer,
+    });
 
-    if (await getChainId() !== '31337') {
-        await hre.run('verify:verify', {
-            address: priorityFeeLimiter.address,
-        });
-    }
+    // SafeOrderBuilder
+    await deployAndGetContract({
+        contractName: 'SafeOrderBuilder',
+        constructorArgs: [LOP, await orderRegistrator.getAddress()],
+        deployments,
+        deployer,
+    });
 };
 
 module.exports.skip = async () => true;
