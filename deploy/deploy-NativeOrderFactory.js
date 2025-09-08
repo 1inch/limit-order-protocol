2310de313edeeb336b13b0e27b6f21e8546c3250
SevenSwen <tihonenko.v.v@gmail.com>
2025-09-08 20:21:25 +0700

    add impementation address log

--- deploy/deploy-NativeOrderFactory.js
+++ deploy/deploy-NativeOrderFactory.js
@@ -1,5 +1,6 @@
 const hre = require('hardhat');
 const { getChainId, network } = hre;
+const { deployAndGetContract } = require('@1inch/solidity-utils');
 
 const wethByNetwork = {
     hardhat: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
@@ -20,15 +21,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running deploy script');
     console.log('network id ', await getChainId());
 
-    const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const nativeOrderFactory = await deploy('NativeOrderFactory', {
-        from: deployer,
-        args: [wethByNetwork[network.name], lop[network.name], accessToken[network.name], 60, '1inch Aggregation Router', '6'],
+    const nativeOrderFactory = await deployAndGetContract({
+        contractName: 'NativeOrderFactory',
+        constructorArgs: [wethByNetwork[network.name], lop[network.name], accessToken[network.name], 60, '1inch Aggregation Router', '6'],
+        deployments,
+        deployer,
     });
+    console.log('NativeOrderFactory deployed to:', await nativeOrderFactory.getAddress());
 
-    console.log('NativeOrderFactory deployed to:', nativeOrderFactory.address);
+    console.log('NativeOrderImpl deployed to:', await nativeOrderFactory.IMPLEMENTATION());
 };
 
-module.exports.skip = async () => true;
+module.exports.skip = async () => false;
