8714b6d641f45490602a28affb7d13e83bdfb4f7
SevenSwen <tihonenko.v.v@gmail.com>
2025-09-10 21:58:34 +0700

    fix script

--- deploy/deploy-NativeOrderFactory.js
+++ deploy/deploy-NativeOrderFactory.js
@@ -3,19 +3,11 @@ const { getChainId, network } = hre;
 const { deployAndGetContract } = require('@1inch/solidity-utils');
 
 const wethByNetwork = {
-    hardhat: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     mainnet: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
 };
 
-const lop = {
-    hardhat: '0x111111125421cA6dc452d289314280a0f8842A65',
-    mainnet: '0x111111125421cA6dc452d289314280a0f8842A65',
-};
-
-const accessToken = {
-    hardhat: '0xAccE550000863572B867E661647CD7D97b72C507',
-    mainnet: '0xAccE550000863572B867E661647CD7D97b72C507',
-};
+const ROUTER_V6_ADDR = '0x111111125421ca6dc452d289314280a0f8842a65';
+const ACCESS_TOKEN_ADDR = '0xAcce5500000f71A32B5E5514D1577E14b7aacC4a';
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('running deploy script');
@@ -25,7 +17,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const nativeOrderFactory = await deployAndGetContract({
         contractName: 'NativeOrderFactory',
-        constructorArgs: [wethByNetwork[network.name], lop[network.name], accessToken[network.name], 60, '1inch Aggregation Router', '6'],
+        constructorArgs: [wethByNetwork[network.name], ROUTER_V6_ADDR, ACCESS_TOKEN_ADDR, 60, '1inch Aggregation Router', '6'],
         deployments,
         deployer,
         skipVerify: true,
